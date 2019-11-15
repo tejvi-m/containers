7a46df2d20fb5b57301381d8c939d6761a25c964
Tejvi M <tejvimstays@gmail.com>
2019-11-15 13:43:44 +0530

    working bash shell, with uts working.

--- cntrs.c
+++ cntrs.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #define _GNU_SOURCE
 #include <sched.h>
 #include <unistd.h>
@@ -14,24 +13,48 @@
 #include <getopt.h>
 
 
+#define die(msg)                                      \
+do {                                                  \
+    perror(msg);                                      \
+    exit(EXIT_FAILURE);                               \
+} while (0)
+
 struct child{
   char *hostname;
   char **args;        // command to be executed + other args
   int pipe_fd[2];    // for communication between parent and child
-}
+};
 
 /* to clone stack for the clone call */
 struct stackClone{
   char stack[4096];
   char ptr[0];
-}
+};
 
-static int runContainer(void arg*){
-  
+static int runContainer(void *arg){
+  struct child *args = (struct child *) arg;
+  char ch;
+  int err = 0;
+
+  close(args->pipe_fd[1]);
+
+  if (read(args->pipe_fd[0], &ch, 1) != 0) {
+      die("Failure in child: read from pipe returned != 0\n");
+  }
+
+  sethostname(args -> hostname, strlen(args -> hostname));
+
+  printf("Executing %s\n", args->args[0]);
+
+  err = execvp(args->args[0], args->args);
+
+  if (0 != err) {
+      printf("execvp error: %s\n", strerror(errno));
+  }
 }
 
 int main(int argc, char* argv[]){
-    int flags = SIGCHILD | CLONE_NEWUTS;
+    int flags = SIGCHLD | CLONE_NEWUTS ;
     // sigchild - termination signal
     // clone newuts - namespace for unix time sharing. set a new host name.
     // TODO add more flags for more namespaces.
@@ -39,18 +62,18 @@ int main(int argc, char* argv[]){
     pid_t container;
 
     struct child childProc;
-    struct clone_stack cloneStack;
+    struct stackClone cloneStack;
 
     childProc.hostname = "container";
-    childProc.args = &argv[2];
-    // optind - from getOpt.h
-    // index of the next element thats yet to be processed in argv.
+    childProc.args = &argv[1];
+  
+    if (pipe(childProc.pipe_fd) == -1) die("pipe");
 
-    container = clone(runContainer, stack.ptr, flags, &childProc);
+    container = clone(runContainer, cloneStack.ptr, flags, &childProc);
     // clone - sched.h
     // TODO read more of the manpage
 
-    if(container = -1) die("clone");
+    if(container == -1) die("clone");
 
     printf("child created with PID: %ld\n", (long) container);
 
