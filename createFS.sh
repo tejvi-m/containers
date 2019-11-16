666045a559c5e8c753386924ffdd648cafbc5b67
Tejvi M <tejvimstays@gmail.com>
2019-11-16 14:51:25 +0530

    bash script to create a copy of the filesystem for the container to use as an image. setup passwordless login

---
+++ createFS.sh
@@ -0,0 +1,7 @@
+# usage : createFS <path_to_iso>
+cd
+mkdir temp
+sudo mount -o loop $1 temp/
+sudo unsquashfs -d rootfs/ temp/install/filesystem.squashfs
+
+#set up passwordless login by removing the x in the first line of rootfs/etc/passwd file
